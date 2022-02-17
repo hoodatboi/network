8a7b6a76271d29d34cf465d7e3d3ae5ee8980add
hoodatboi <Hoooody@yandex.ru>
2022-02-17 14:23:28 +0300

    Commit

---
+++ src/components/Profile.jsx
@@ -0,0 +1,18 @@
+const Profile = () => {
+  return (
+    <div className="Content">
+      <div>
+        <img src="https://picfiles.alphacoders.com/297/297577.jpg" />
+      </div>
+      <div>ava + description</div>
+      <div>
+        my posts
+        <div>new post</div>
+        <div>post 1</div>
+        <div>post 2</div>
+      </div>
+    </div>
+  );
+};
+
+export default Profile;
